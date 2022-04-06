b8aaa93736d56c8382c33d545c9c032253f1cce7
lauralardies <lrodrlar@myuax.com>
2022-04-06 21:46:01 +0200

    parte2

--- archivos.py
+++ archivos.py
@@ -1,7 +1,7 @@
+from asyncio.windows_events import NULL
 from operator import itemgetter
 
 class Calificaciones():
-
     def leer_fichero(self, fichero):
         with open(fichero, "r") as archivo:
             next(archivo, None) # Esta función nos permite saltarnos la primera fila, donde aparece el encabezado
@@ -23,7 +23,28 @@ class Calificaciones():
                 ])
                 lista_dict.append(diccionario) # Añadimos cada diccionario generado cada vez que se ejecuta el bucle for a la variable lista
         
-        return sorted(lista_dict, key=itemgetter("Apellidos"))
+        return sorted(lista_dict, key=itemgetter("Apellidos")) # Nos permite ordenar el diccionario alfabéticamente por apellidos
+
+    def nota_final(self, lista):
+        for i in range(0, len(lista)):
+            parcial1 = (lista[i]["Parcial1"]).replace(",", ".")
+            parcial2 = (lista[i]["Parcial2"]).replace(",", ".")            
+            practicas = (lista[i]["Prácticas"]).replace(",", ".")
+
+            if any(chr.isdigit() for chr in parcial1) == False:
+                parcial1 = 0.0
+            
+            if any(chr.isdigit() for chr in parcial2) == False:
+                parcial2 = 0.0
+
+            if any(chr.isdigit() for chr in practicas) == False:
+                practicas = 0.0
+
+            lista[i]["NotaFinal"] = float(parcial1) * 0.3 + float(parcial2) * 0.3 + float(practicas) * 0.4
+
+        return lista
+
 
 test = Calificaciones()
-print(test.leer_fichero("calificaciones.csv"))
+lista = test.leer_fichero("calificaciones.csv")
+test.nota_final(test.leer_fichero("calificaciones.csv"))
